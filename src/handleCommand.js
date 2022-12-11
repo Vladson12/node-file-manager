5c193826b2f4f5749850314ad2a33678fad18ca1
Vladislav Magomedov <vladdmagg94@yandex.ru>
2022-12-11 22:24:35 +0300

    feat: add move up to parent directory

--- src/handleCommand.js
+++ src/handleCommand.js
@@ -1,18 +1,28 @@
 import { Transform } from "stream";
+import { moveUp } from "./move.js";
+import { EOL } from "os";
+import { cwd } from "process";
 
 export const handle = new Transform({
   transform(chunk, encoding, callback) {
     const [command, ...args] = chunk.toString().trim().split(" ");
 
-    let result;
-    switch (command) {
-      case ".exit":
-        process.emit("beforeexit");
-        break;
-      default:
-        result = "nothing happens!\n";
-        break;
+    try {
+      switch (command) {
+        case ".exit":
+          process.emit("beforeexit");
+          break;
+        case "up":
+          moveUp();
+          break;
+        default:
+          throw new Error(`${command}: command not found`);
+      }
+    } catch (err) {
+      console.log(`Operation failed: ${err.message}`);
     }
-    callback(null, result);
+
+    console.log(`${EOL}You are currently in ${cwd()}`);
+    callback();
   },
 });
